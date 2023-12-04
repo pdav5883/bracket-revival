cf4ef4e82920b06285687cbe56e23a0861479703
Peter Davison <davison.pl@gmail.com>
2023-12-04 00:54:57 -0500

    Pick page with clicks to populate bracket picks

--- frontend/scripts/pick.js
+++ frontend/scripts/pick.js
@@ -3,6 +3,54 @@ const api_url = "http://0.0.0.0:5000/start"
 $(document).ready(function() {
   $("#gobutton").on("click", populateRoundStart)
 })
+var testi = 0
+
+// this needs to happen after bracket table is loaded
+function addBracketListeners() {
+  // all spans in bracket
+  $("[id^=span_]").each(function() {
+    $(this).on("click", function() {
+      
+      // click will fire event even if already selected
+      if ($(this).hasClass("winnerspan")) {
+	return
+      }
+
+      // toggle winner for this game competition
+      $(this).addClass("winnerspan")
+      $("#" + $(this).prop("opponentId")).removeClass("winnerspan")
+
+      // for next round only, want to add in name of this winner 
+      let nextId = $(this).prop("nextId")
+      if (nextId !== null) {
+	$("#" + nextId).text($(this).text())
+
+	// only keep going if we changed a winner
+	if ($("#" + nextId).hasClass("winnerspan")) {
+	  $("#" + nextId).removeClass("winnerspan")
+	  nextId = $("#" + nextId).prop("nextId")
+	}
+	else {
+	  nextId = null
+	}
+      }
+
+      // for all later rounds, just clear winner/name if this was already winner
+      while (nextId !== null) {
+	$("#" + nextId).text("---")
+
+	if ($("#" + nextId).hasClass("winnerspan")) {
+	  $("#" + nextId).removeClass("winnerspan")
+	  nextId = $("#" + nextId).prop("nextId")
+	}
+	else {
+	  nextId = null
+	}
+      }
+    })
+  })
+}
+
 
 function populateRoundStart() {
   const year = $("#yearinput").val()
@@ -28,33 +76,43 @@ function populateRoundStart() {
 	let tableRow = table.insertRow()
 	bracketRow.forEach(bracketCell => {
 	  if (bracketCell !== null) {
-	    buildMatchup(tableRow.insertCell(), bracketCell)
+	    buildPickMatchup(tableRow.insertCell(), bracketCell)
 	  }
 	})
       })
 
+      // adds the on-click listeners to populate picks in bracket
+      addBracketListeners()
     }
   })
 }
 
 
-function buildMatchup(tableCell, bracketCell) {
+function buildPickMatchup(tableCell, bracketCell) {
+  
+  const rnd = bracketCell.index[0]
+  const gm = bracketCell.index[1]
+  
+  const nextRnd = bracketCell.nextIndex[0]
+  const nextGm = bracketCell.nextIndex[1]
+  const nextSpan = gm % 2 // tells us whether this goes into top or bottom span entry of next game
+  const nextSpanId = nextRnd === null ? null : "span_" + nextRnd + "_" + nextGm + "_" + nextSpan
+
   tableCell.rowSpan = bracketCell.rowSpan
+  tableCell.id = "cell_" + rnd + "_" + gm
+
   let t0 = document.createElement("span")
   t0.textContent = bracketCell.teams[0] === null ? "---" : "#" + bracketCell.seeds[0] + " " + bracketCell.teams[0]
+
   let t1 = document.createElement("span")
   t1.textContent = bracketCell.teams[1] === null ? "---" : "#" + bracketCell.seeds[1] + " " + bracketCell.teams[1]
 
-  if (bracketCell.result == 0) {
-    t0.classList.add("winnerspan")
-    t0.textContent += " (" + bracketCell.score[0] + ")"
-    t1.textContent += " (" + bracketCell.score[1] + ")"
-  }
-  else if (bracketCell.result == 1) {
-    t1.classList.add("winnerspan")
-    t0.textContent += " (" + bracketCell.score[0] + ")"
-    t1.textContent += " (" + bracketCell.score[1] + ")"
-  }
+  t0.id = "span_" + rnd + "_" + gm + "_" + "0"
+  t0.opponentId = "span_" + rnd + "_" + gm + "_" + "1"
+  t0.nextId = nextSpanId
+  t1.id = "span_" + rnd + "_" + gm + "_" + "1"
+  t1.opponentId = "span_" + rnd + "_" + gm + "_" + "0"
+  t1.nextId = nextSpanId
 
   tableCell.appendChild(t0)
   tableCell.appendChild(document.createElement("br"))
@@ -78,7 +136,9 @@ function buildStartBracketArray(startGames) {
   let i
   for (i = 0; i < numRows; i++) {
     game = startGames[i]
-    game["rowSpan"] = 1
+    game.rowSpan = 1
+    game.index = [0, i] // index contains round, game-within round index for id referencing later
+    game.nextIndex = numCols == 1 ? [null, null] : [1, Math.floor(i / 2)] // index of the game where winner goes
     bracketArray[i][0] = game
   }
 
@@ -86,7 +146,9 @@ function buildStartBracketArray(startGames) {
   for (rnd = 1; rnd < numCols; rnd++) {
     for (i = 0; i < numRows; i += 2 ** rnd) {
       game = {"teams": [null, null],
-              "rowSpan": 2 ** rnd}
+              "rowSpan": 2 ** rnd,
+              "index": [rnd, i / (2 ** rnd)],
+              "nextIndex": numCols == (rnd + 1) ? [null, null] : [rnd + 1, Math.floor(i / (2 ** (rnd + 1)))]}
       bracketArray[i][rnd] = game
     }
   }
