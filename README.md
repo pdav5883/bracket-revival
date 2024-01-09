2b806005bedc7533f8fa718b45b8bed377669ab8
Peter <davison.pl@gmail.com>
2024-01-09 16:58:38 -0500

    Update bracket calc to compute indices first

--- README.md
+++ README.md
@@ -17,8 +17,14 @@ Work in progress
 - Output: {"points": [flat list of points per game, 0 if game not played]}
 #### get_bracket
 - GET: Reads data files and returns nested list of game results (no picks)
+- **WAS**
 - Input: year, cid, completed_rounds (opt)
 - Output: [[{"teams": [a,b], "seeds": [a,b], "score": [a,b], "result": 0/1}, (round 0 game 1), ...], [(round 1 game 0), (round 1 game 1), ...], ...]
+- **IS**: add pick/points info if pid is present
+- GET: Reads data files and returns nested list of results, plus nested picks and points if pid is present.
+- Input: year, cid, pid (opt), completed_rounds (opt)
+- Output: [[{"teams": [a,b], "seeds": [a,b], "score": [a,b], "result": 0/1, "picks": [[],[]], "points": [a,b]}, (r0g1), ...], [(r1g0), (r1g1), ...], ...].
+- picks and points fields are structured for frontend rendering. i.e. the picks values will not be one of the "teams" values. Picks is who was picked to be **playing** in this game, and points is how many points were awarded for correctly picking who is in the game.
 #### get_round_start
 - GET: Returns list of games starting in given round to populate bracket
 - Input: year, cid, round_start
@@ -63,6 +69,12 @@ Work in progress
 	- Prev-prev: pick = result
 	- points MULT
 ## Questions/Misc Thoughts
+- How much data does the backend send back for get_bracket?
+	- Backend will look at three sources to determine how much data to send.
+	- 1. completed_rounds in competition.json. If completed_rounds = 1, then it will show all available results and picks for round 0.
+	- 2. completed_rounds in query param. Same behavior as #1
+	- 3. Picks made by pid. If pid has only submitted picks for round 0 and round 1, this is equivalent to setting completed_rounds = 2. 
+	- Rationale: this pushes complexity from the frontend to the backend, where I can write better code. This way I can also use completed_rounds in competition.json to hide picks from other players until a round starts. For example, with completed_rounds = 0, players wouldn't be able to see anyone elses picks. Then once the first rounds starts I'd set completed_rounds = 1 and all picks would be visible. Downside is that players would not be able to see their own picks until the round starts. (Could get around this by allowing edits).
 - If you change your pick to have a team keep going when you had them going out, should you get all the bonus?
 	- No bonus should be awarded based on when the pick is made, not whether you correctly picked the team to this point. If you switch a pick in the current round, that pick should always be worth 1.
 
--- lambdas/get_bracket/lambda_function.py
+++ lambdas/get_bracket/lambda_function.py
@@ -8,11 +8,19 @@ def lambda_handler(event, context):
     """
     GET request
 
-    Input: year, cid, completed_rounds (so that frontend can show earlier games states)
-    Output: [round 0 games [{teams: [], seeds: [], score: [], result:}]]
+    Input: year, cid, pid (opt), completed_rounds (opt)
+    Output: nested list of games
+            [[{"teams": [a,b], "seeds": [a,b], "score": [a,b], "result": 0/1, "picks": [[a,a,a], [b,b,b]], "points": [0/1/2, 0/1/2]}, (r0g1), ...],
+             [(r1g0), (r1g1), ...],
+             ...]
+
+    "picks" and "points" are null is pid is missing
+
+    "picks" respresents who we picked to be *playing* in the game, not who we picked to win the game.
     """
     year = event["queryStringParameters"].get("year", None)
     cid = event["queryStringParameters"].get("cid", None)
+    pid = event["queryStringParameters"].get("pid", None)
     completed_rounds_query = event["queryStringParameters"].get("completed_rounds", None)
 
     # TODO: assert arguments exist
@@ -20,67 +28,52 @@ def lambda_handler(event, context):
     results_key = prefix + year + "/results.json"
     teams_key = prefix + year + "/teams.json"
     competition_key = prefix + year + "/" + cid + "/competition.json"
- 
+
     results_dict = read_file(results_key)
-    results = results_dict.get("results")
-    scores = results_dict.get("scores")
+    results = results_dict["results"]
+    scores = results_dict["scores"]
 
     teams = read_file(teams_key)
     names = [t["name"] for t in teams]
     seeds = [t["seed"] for t in teams]
 
     competition = read_file(competition_key)
-    
-    if completed_rounds_query is None:
-        completed_rounds = competition.get("completed_rounds")
-    else:
-        completed_rounds = min(competition.get("completed_rounds"), int(completed_rounds_query))
 
-    # set all results beyond played rounds to None
+    if pid is None:
+        player = None
+    else:
+        player_key = prefix + year + "/" + cid + "/" + pid + ".json"
+        player = read_file(player_key)
+
+
+    # don't show a player results past completed_rounds or beyond the picks they've made
+    completed_rounds = competition["completed_rounds"]
+
+    if completed_rounds_query is not None:
+        completed_rounds = min(completed_rounds, int(completed_rounds_query))
+
+    if player is not None:
+        completed_rounds = min(completed_rounds, len(player["picks"]))
+
+    # set all results beyond played rounds to None and hide picks beyond round
     if completed_rounds < NUMROUNDS:
         played_games = sum(GAMES_PER_ROUND[0:completed_rounds])
         for i in range(played_games, NUMGAMES):
             results[i] = None
             scores[i] = [None, None]
 
+        if player is not None:
+            player["picks"] = player["picks"][0:completed_rounds]
+
     games = []
+    
+    abs_inds = make_absolute_bracket(results)
 
-    # create flat list of bracket
-    for i in range(NUMGAMES):
-        i_prev_upper, i_prev_lower = PREV_GAME[i]
-
-        if i_prev_upper is None:
-            name_upper = names[2 * i]
-            name_lower = names[2 * i + 1]
-            seed_upper = seeds[2 * i]
-            seed_lower = seeds[2 * i + 1]
-
-        else:
-            game_upper = games[i_prev_upper]
-            result_upper = game_upper["result"]
-
-            if result_upper is None:
-                name_upper = None
-                seed_upper = None
-            else:
-                name_upper = game_upper["teams"][result_upper]
-                seed_upper = game_upper["seeds"][result_upper]
-
-            game_lower = games[i_prev_lower]
-            result_lower = game_lower["result"]
-
-            if result_lower is None:
-                name_lower = None
-                seed_lower = None
-            else:
-                name_lower = game_lower["teams"][result_lower]
-                seed_lower = game_lower["seeds"][result_lower]
-
-        game = {"teams": [name_upper, name_lower],
-                "seeds": [seed_upper, seed_lower],
+    for i, (i_upper, i_lower) in enumerate(abs_inds):
+        game = {"teams": [names[i_upper] if i_upper is not None else None, names[i_lower] if i_lower is not None else None],
+                "seeds": [seeds[i_upper] if i_lower is not None else None, seeds[i_lower] if i_lower is not None else None],
                 "score": scores[i],
                 "result": results[i]}
-
         games.append(game)
 
     # nest flat list into rounds
@@ -93,6 +86,58 @@ def lambda_handler(event, context):
         games_nested.append(games_round)
 
     return games_nested
+
+
+def make_absolute_bracket(results, picks=None):
+    """
+    Converts flat list of 0/1 results and/or picks into flat list of upper/lower absolute indices into teams
+
+    - results must start from round0 and continue through round i (or be empty)
+    - if present picks must start at round i+1 (or zero if results empty) and continue through final round.
+
+    Returns list of absolute upper/lower indices for each game, with None for unplayed
+
+    Returns None if something goes wrong (TODO: do something else here)
+    """
+    # rel_inds is 0/1 result/pick from previous game
+    if picks is not None:
+        rel_inds = results + picks
+    else:
+        rel_inds = results
+
+    if len(rel_inds) != NUMGAMES:
+        return None
+
+    abs_inds = []
+
+    # create flat list of bracket indices
+    for i in range(NUMGAMES):
+        i_prev_upper, i_prev_lower = PREV_GAME[i]
+
+        if i_prev_upper is None:
+            i_upper = 2 * i
+            i_lower = 2 * i + 1
+
+        else:
+            abs_upper = abs_inds[i_prev_upper]
+            rel_upper = rel_inds[i_prev_upper]
+
+            if rel_upper is None:
+                i_upper = None
+            else:
+                i_upper = abs_upper[rel_upper]
+
+            abs_lower = abs_inds[i_prev_lower]
+            rel_lower = rel_inds[i_prev_lower]
+
+            if rel_lower is None:
+                i_lower = None
+            else:
+                i_lower = abs_lower[rel_lower]
+
+        abs_inds.append((i_upper, i_lower))
+
+    return abs_inds
 
 
 def read_file(key):
