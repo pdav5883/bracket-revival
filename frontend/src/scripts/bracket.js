6f52587ebf3f211261e1870f699608c683583204
Peter Davison <davison.pl@gmail.com>
2024-02-27 16:39:44 -0500

    Show constant number of checkmarks per round, all x's if zero points

--- frontend/src/scripts/bracket.js
+++ frontend/src/scripts/bracket.js
@@ -251,10 +251,10 @@ function populateBracket(args) {
           const xmark = "<svg xmlns='http://www.w3.org/2000/svg' height='18' width='18' viewBox='0 0 512 512'><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill='#e21212' d='M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z'/></svg>"
 
           if (numCorrect > 0) {
-            return playerHTML + " " + checkmark.repeat(numCorrect)
+            return playerHTML + " " + xmark.repeat(context.roundIndex + 1 - numCorrect) + checkmark.repeat(numCorrect)
           }
           else if (numCorrect < 0) {
-            return playerHTML + " " + xmark
+            return playerHTML + " " + xmark.repeat(context.roundIndex + 1)
           }
           else {
             return playerHTML
