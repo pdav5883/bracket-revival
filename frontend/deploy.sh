d8647c7d91570b6e6f1151f4d165fd2c8c2186dd
Peter Davison <davison.pl@gmail.com>
2024-03-16 10:46:06 -0400

    Deploy frontend to AWS

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 sync . s3://{bucket-name} --cache-control="max-age=21600" --exclude="*" --include="*.html" --include="*.css" --include="*.js"
-aws cloudfront create-invalidation --distribution-id {dist-id} --paths "/*"
+aws s3 sync ./dist s3://bracket-revival-public --cache-control="max-age=21600" --exclude="*" --include="*.html" --include="*.css" --include="*.js"
+aws cloudfront create-invalidation --distribution-id EAYYH2TLICFBU --paths "/*"
