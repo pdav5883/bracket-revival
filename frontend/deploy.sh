6bc2e295930090165134da11ba08a49f474749fa
Peter Davison <davison.pl@gmail.com>
2024-03-18 21:48:19 -0400

    Correct error in frontend deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 sync ./dist s3://bracket-revival-public --cache-control="max-age=120" --exclude="*" --include="*.html" --include="*.css" --include="*.js" --include"*.png"
+aws s3 sync ./dist s3://bracket-revival-public --cache-control="max-age=120" --exclude="*" --include="*.html" --include="*.css" --include="*.js" --include="*.png"
 aws cloudfront create-invalidation --distribution-id EAYYH2TLICFBU --paths "/*"
