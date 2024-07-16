178c1609224d35226d62357e6b24371e0f0a05f9
Peter Davison <davison.pl@gmail.com>
2024-07-15 21:44:16 -0400

    CFN start, buckets, two lambdas, and common lambda layer

---
+++ infrastructure/deploy.sh
@@ -0,0 +1,28 @@
+################
+# This script deploys the AWS resources required for bowl-pickem using CloudFormation.
+# The input to the script is the name of the JSON parameters file used to define
+# parameter values in the CFN stack.
+#
+# When the CFN stack is deployed, the lambda functions are populated with placeholder
+# code, so this scripts also calls the deploy scripts for each lambda to upload code.
+################
+
+echo "Deploying bracket-revival cloudformation with params from ${1}"
+
+aws cloudformation deploy \
+  --template-file ./bracket-revival-cfn.yaml \
+  --stack-name bracket-revival \
+  --parameter-overrides file://${1} \
+  --capabilities CAPABILITY_NAMED_IAM \
+  # --no-execute-changeset
+
+#cd ../lambdas
+
+#for d in */
+#do
+#  echo "Deploying lambda update from ${d}"
+#  cd $d
+#  sh deploy.sh
+#  cd ..
+#done
+
