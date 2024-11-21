ab63319a3340ad52d84cb98c5d58edfcc215a095
Peter Davison <davison.pl@gmail.com>
2024-11-20 20:23:59 -0500

    Updated deploy frontend to use same as bowl-pickem and lambdas

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,18 @@
 const path = require("path")
 const HtmlWebpack = require("html-webpack-plugin")
 const CopyWebpack = require("copy-webpack-plugin")
+const { execSync } = require('child_process')
+
+// Get CloudFormation parameters
+const cfParams = Object.fromEntries(
+  execSync('./get-cf-params.sh', { encoding: 'utf-8' })
+    .trim()
+    .split('\n')
+    .map(line => {
+      const [key, value] = line.split('=')
+      return [key, JSON.stringify(value)]
+    })
+)
 
 module.exports = {
   entry: {
@@ -107,6 +119,19 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"]
       },
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'string-replace-loader',
+          options: {
+            multiple: Object.entries(cfParams).map(([key, value]) => ({
+              search: key,
+              replace: value,
+              flags: 'g'
+            }))
+          }
+        }]
+      }
     ]
   }
 }
