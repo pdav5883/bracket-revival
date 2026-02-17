547f54e7e54233655eef1cb4005cc077de9414b4
Peter Davison <davison.pl@gmail.com>
2026-02-16 21:43:35 -0500

    Underdog points complete in common layer

--- lambdas/bracket_common/points.py
+++ lambdas/bracket_common/points.py
@@ -1,26 +1,80 @@
 from bracket_common import tournament as trn
-
+import math
 
 def calc_points_underdog_revival(picks, results):
     """
     Return points_base, points_multiplier
     """
+    points_multiplier = calc_points_revival(picks, results)
+    points_base = trn.NUMGAMES * [0]
 
-def calc_best_seed(results):
+    # the seed of the winner for each game
+    winner_seed = []
+    for i_abs, res in enumerate(results):
+        if res is None:
+            winner_seed.append(None)
+        elif trn.PREV_GAME[i_abs][res] is None:
+            winner_seed.append(trn.SEEDS[i_abs][res])
+        else:
+            winner_seed.append(winner_seed[trn.PREV_GAME[i_abs][res]])
+
+    best_matchup_all = calc_best_matchup(results)
+
+    rnd = 0
+
+    for i_abs, multiplier in enumerate(points_multiplier):
+        # did not pick game correctly
+        if multiplier == 0:
+            continue
+
+        if i_abs == sum(trn.GAMES_PER_ROUND[:rnd+1]):
+            rnd += 1
+
+        back_rnd = int(math.log2(multiplier))
+        i_rel = i_abs - sum(trn.GAMES_PER_ROUND[:rnd-back_rnd])
+        best_seed = best_matchup_all[rnd - back_rnd][i_rel][1 - results[i_abs]]
+        points_base[i_abs] = calc_base_underdog_revival(winner_seed[i_abs], best_seed)
+
+    return points_base, points_multiplier
+
+
+def calc_base_underdog_revival(seed_winner, seed_loser):
+    upset = int(seed_winner) - int(seed_loser)
+    
+    if upset <= 2:
+        return 1
+    elif upset <= 5:
+        return 2
+    elif upset <= 8:
+        return 3
+    elif upset <= 11:
+        return 4
+    else:
+        return 5
+
+
+def calc_best_matchup(results):
     """
-    For each game, calculate the best possible seed that could be playing in the game
+    For each game, calculate the best possible seeds that could be playing in the game
     based on the rnd that picks were made.
-    results = [0, 1, ..., None, None] 
-    Outputs: [[1, 8, 5, 4, 6, 3, 7, 2,...,1, 1, 1, 1, 1, 1, 1]
-    ] 
+
+    Calculating the best matchup is necessary because the best seed that a team can play in a 
+    given game depends on whether that team is the upper or lower team. For example, the best
+    team a 16 seed can play in the second round is an 8 (since 16 would beat 1 in 1st rnd). However
+    in that same second round game, that 8 seed could be playing a 1 seed. In order to calculate
+    the base points in future rounds, the upper and lower best seeds must be tracked.
+
+    results = [0, 1, ..., None, None]
+    Outputs: [[1, 8, 5, 4, 6, 3, 7, 2,...,1, 1, 1, 1, 1, 1, 1] (32),...
+    ]
     """
     # round_start_best_seeds = [min(matchup) for matchup in list(trn.SEEDS)]
-    round_start_seeds = list(trn.SEEDS)
-    best_seeds = []
+    round_start_matchups = list(trn.SEEDS)
+    best_matchups = []
 
     for pick_rnd in range(trn.NUMROUNDS):
         # best_seeds_pick_rnd = list(round_start_best_seeds)
-        best_seeds_pick_rnd = [min(matchup) for matchup in round_start_seeds]
+        best_matchups_rnd = list(round_start_matchups)
         num_prev_games = sum(trn.GAMES_PER_ROUND[:pick_rnd])
         num_this_games = trn.GAMES_PER_ROUND[pick_rnd]
 
@@ -29,13 +83,13 @@ def calc_best_seed(results):
         stop = trn.NUMGAMES
         for i_abs in range(start, stop):
             prev0, prev1 = trn.PREV_GAME[i_abs]
-            prev0_seed = best_seeds_pick_rnd[prev0 - num_prev_games]
-            prev1_seed = best_seeds_pick_rnd[prev1 - num_prev_games]
-            best_seeds_pick_rnd.append(min(prev0_seed, prev1_seed))
+            prev0_matchup = best_matchups_rnd[prev0 - num_prev_games]
+            prev1_matchup = best_matchups_rnd[prev1 - num_prev_games]
+            best_matchups_rnd.append((min(prev0_matchup), min(prev1_matchup)))
 
-        best_seeds.append(best_seeds_pick_rnd)
+        best_matchups.append(best_matchups_rnd)
 
-        next_round_start_seeds = list()
+        next_round_start_matchups = list()
         num_next_games = trn.GAMES_PER_ROUND[pick_rnd + 1] if num_this_games > 1 else 0
         start = num_prev_games + num_this_games
         stop = num_prev_games + num_this_games + num_next_games
@@ -45,18 +99,17 @@ def calc_best_seed(results):
 
             # if there is an unfinished game in this round, then not possible to make this round of picks
             if res0 is None or res1 is None:
-                return best_seeds
-            
-            winner0 = round_start_seeds[prev0 - num_prev_games][res0]
-            winner1 = round_start_seeds[prev1 - num_prev_games][res1]
-            next_round_start_seeds.append((winner0, winner1))
+                return best_matchups
 
-        round_start_seeds = list(next_round_start_seeds)
+            winner0 = round_start_matchups[prev0 - num_prev_games][res0]
+            winner1 = round_start_matchups[prev1 - num_prev_games][res1]
+            next_round_start_matchups.append((winner0, winner1))
 
-    return best_seeds
+        round_start_matchups = list(next_round_start_matchups)
+    return best_matchups
 
 
-def calc_points_revival(picks, results, K=2):
+def calc_points_revival(picks, results):
     """
     Picks are nested, full picks for first round and on, full picks second round and on
     """
@@ -99,7 +152,7 @@ def calc_points_revival(picks, results, K=2):
                         break
 
                     # if we've made it to None child, then we picked i_abs game correctly in previous round
-                    points[i_abs] *= K
+                    points[i_abs] *= 2
 
                     rnd_back -= 1
 
--- lambdas/test_lambdas.py
+++ lambdas/test_lambdas.py
@@ -1,105 +1,176 @@
 import json
+import pytest
+from bracket_common import points
+from bracket_common import tournament as trn
 
-from UpdatePicks import lambda_function as update_picks
-from GetBracket import lambda_function as get_bracket
-from GetRoundStart import lambda_function as get_round_start
-from UpdateScoreboard import lambda_function as update_scoreboard
-from GetScoreboard import lambda_function as get_scoreboard
-from GetCompetitions import lambda_function as get_competitions
-from AddElement import lambda_function as add_element
-from AdminEdit import lambda_function as admin_edit
-from AdminAuth import lambda_function as admin_auth
-
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
+N = trn.NUMGAMES
 
 
-@app.route("/picks", methods=["POST"])
-def update_function():
-    event = make_event()
-    data = update_picks.lambda_handler(event, None)
+class TestPoints:
+    def best_propagate_round(self, seeds_in):
+        """
+        Flat list of seeds in a round
+        """
+        if len(seeds_in) < 2:
+            return None
+        return [min(upper, lower) for upper, lower in zip(seeds_in[::2], seeds_in[1::2])]
 
-    return jsonify(data)
+    def best_propagate_all(self, round_start):
+        if type(round_start[0]) is tuple:
+            round_start = [seed for matchup in trn.SEEDS for seed in matchup]
+        best_seeds = []
+        round_start = self.best_propagate_round(round_start)
+
+        while round_start is not None:
+            best_seeds.extend(round_start)
+            round_start = self.best_propagate_round(round_start)
+
+        return best_seeds
+
+    @pytest.fixture
+    def start_best_seeds(self):
+        return [1, 8, 5, 4, 6, 3, 7, 2,
+                1, 8, 5, 4, 6, 3, 7, 2,
+                1, 8, 5, 4, 6, 3, 7, 2,
+                1, 8, 5, 4, 6, 3, 7, 2,
+                1, 4, 3, 2, 1, 4, 3, 2,
+                1, 4, 3, 2, 1, 4, 3, 2,
+                1, 2, 1, 2, 1, 2, 1, 2,
+                1, 1, 1, 1, 1, 1, 1]
+
+    def test_helper_function(self, start_best_seeds):
+        assert self.best_propagate_all([3, 6, 8, 14]) == [3, 8, 3]
+        assert self.best_propagate_all(trn.SEEDS) == start_best_seeds
+
+    def test_calc_best(self):
+        no_results = [None] * N
+        assert [
+            [min(m) for m in m_round]
+            for m_round in points.calc_best_matchup(no_results)
+        ] == [self.best_propagate_all(trn.SEEDS)]
+
+        one_result = [1] + [None] * (N - 1)
+        assert [
+            [min(m) for m in m_round]
+            for m_round in points.calc_best_matchup(one_result)
+        ] == [self.best_propagate_all(trn.SEEDS)]
+
+        first_round_complete = (
+            [1] * trn.GAMES_PER_ROUND[0]
+            + [None] * sum(trn.GAMES_PER_ROUND[1:])
+        )
+
+        start_second = [matchup[1] for matchup in trn.SEEDS]
+        assert [
+            [min(m) for m in m_round]
+            for m_round in points.calc_best_matchup(first_round_complete)
+        ] == [
+            self.best_propagate_all(trn.SEEDS),
+            self.best_propagate_all(start_second),
+        ]
+
+        second_round_complete = (
+            [1] * trn.GAMES_PER_ROUND[0]
+            + [0] * trn.GAMES_PER_ROUND[1]
+            + [None] * sum(trn.GAMES_PER_ROUND[2:])
+        )
+        start_third = [seed for seed in start_second[::2]]
+        assert [
+            [min(m) for m in m_round]
+            for m_round in points.calc_best_matchup(second_round_complete)
+        ] == [
+            self.best_propagate_all(trn.SEEDS),
+            self.best_propagate_all(start_second),
+            self.best_propagate_all(start_third),
+        ]
+
+        fifth_round_complete = (
+            [1] * trn.GAMES_PER_ROUND[0]
+            + [0] * trn.GAMES_PER_ROUND[1]
+            + [1] * trn.GAMES_PER_ROUND[2]
+            + [0] * trn.GAMES_PER_ROUND[3]
+            + [1] * trn.GAMES_PER_ROUND[4]
+            + [None] * trn.GAMES_PER_ROUND[5]
+        )
+        start_fourth = [seed for seed in start_third[1::2]]
+        start_fifth = [seed for seed in start_fourth[::2]]
+        start_sixth = [seed for seed in start_fifth[1::2]]
+        assert [
+            [min(m) for m in m_round]
+            for m_round in points.calc_best_matchup(fifth_round_complete)
+        ] == [
+            self.best_propagate_all(trn.SEEDS),
+            self.best_propagate_all(start_second),
+            self.best_propagate_all(start_third),
+            self.best_propagate_all(start_fourth),
+            self.best_propagate_all(start_fifth),
+            self.best_propagate_all(start_sixth),
+        ]
+
+        sixth_round_complete = (
+            [1] * trn.GAMES_PER_ROUND[0]
+            + [0] * trn.GAMES_PER_ROUND[1]
+            + [1] * trn.GAMES_PER_ROUND[2]
+            + [0] * trn.GAMES_PER_ROUND[3]
+            + [1] * trn.GAMES_PER_ROUND[4]
+            + [0] * trn.GAMES_PER_ROUND[5]
+        )
+        assert [
+            [min(m) for m in m_round]
+            for m_round in points.calc_best_matchup(sixth_round_complete)
+        ] == [
+            self.best_propagate_all(trn.SEEDS),
+            self.best_propagate_all(start_second),
+            self.best_propagate_all(start_third),
+            self.best_propagate_all(start_fourth),
+            self.best_propagate_all(start_fifth),
+            self.best_propagate_all(start_sixth),
+        ]
+
+    def test_underdog_revival(self):
+        # no games played
+        results = [None] * N
+        picks = [[0] * N]
+        base = [0] * N
+        mult = [0] * N
+        assert points.calc_points_underdog_revival(picks, results) == (base, mult)
+
+        # everything wrong
+        results = [0] * N
+        picks = [[1] * sum(trn.GAMES_PER_ROUND[i:]) for i in range(trn.NUMROUNDS)]
+        base = [0] * N
+        mult = [0] * N
+        assert points.calc_points_underdog_revival(picks, results) == (base, mult)
+
+        # first 8 picks correct
+        results = [1] * 8 + [None] * (N - 8)
+        picks = [[1] * N]
+        base = [5, 1, 3, 4, 2, 4, 2, 5] + [0] * (N - 8)
+        mult = [1] * 8 + [0] * (N - 8)
+        assert points.calc_points_underdog_revival(picks, results) == (base, mult)
+
+        # 16/1 upset then 16/8 upset
+        results = [1] + [0] * 31 + [0] + [None] * (N - 33)
+        picks = [[1] * N, [0] * (N - 32)]
+        picks[0][32] = 0
+        base = [5] + [0] * 31 + [3] + [0] * (N - 33)
+        mult = [1] + [0] * 31 + [2] + [0] * (N - 33)
+        assert points.calc_points_underdog_revival(picks, results) == (base, mult)
+
+        # get second round pick correct second time
+        results = [1] * 33 + [None] * (N - 33)
+        picks = [[0] * N, [1] + [0] * (N - 33)]
+        base = [0] * N
+        base[32] = 1
+        mult = [0] * N
+        mult[32] = 1
+        assert points.calc_points_underdog_revival(picks, results) == (base, mult)
 
 
-@app.route("/bracket", methods=["GET"])
-def bracket_function():
-    event = make_event()
-    data = get_bracket.lambda_handler(event, None)
+# SEEDS = 4 * [(1, 16), (8, 9), (5, 12), (4, 13), (6, 11), (3, 14), (7, 10), (2, 15)]
 
-    return jsonify(data)
-
-
-@app.route("/start", methods=["GET"])
-def start_function():
-    event = make_event()
-    data = get_round_start.lambda_handler(event, None)
-
-    return jsonify(data)
-
-
-@app.route("/sync", methods=["POST","GET"])
-def sync_function():
-    event = make_event()
-    data = update_scoreboard.lambda_handler(event, None)
-
-    return jsonify(data)
-
-
-@app.route("/scoreboard", methods=["GET"])
-def scoreboard_function():
-    event = make_event()
-    data = get_scoreboard.lambda_handler(event, None)
-
-    return jsonify(data)
-
-
-@app.route("/competitions", methods=["GET"])
-def competitions_function():
-    event = make_event()
-    data = get_competitions.lambda_handler(event, None)
-
-    return jsonify(data)
-
-
-@app.route("/add", methods=["PUT", "GET"])
-def add_element_function():
-    event = make_event()
-    data = add_element.lambda_handler(event, None)
-
-    return jsonify(data)
-
-
-@app.route("/admin", methods=["POST"])
-def admin_edit_function():
-    event = make_event()
-    
-    # auth is automatic true
-    event["headers"] = {"authorization": "test"}
-    auth = admin_auth.lambda_handler(event, None)
-
-    if auth["isAuthorized"]:
-        data = admin_edit.lambda_handler(event, None)
-    else:
-        data = "Invalid authorization"
-
-    return jsonify(data)
-
-
-def make_event():
-    if request.method == "GET":
-        event = {"queryStringParameters": dict(request.args)}
-    elif request.method == "POST":
-        event = {"body": json.dumps(request.json)}
-    elif request.method == "PUT":
-        event = {"body": json.dumps(request.json)}
-    else:
-        event = {}
-
-    return event
-
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+# 0-2: 1 pts
+# 3-5: 2 pts
+# 6-8: 3 pts
+# 9-11: 4 pts
+# 12-15: 5 pts
