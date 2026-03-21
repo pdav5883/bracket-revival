911d0dbffdeb3f9815bed30c55664040f06b01c0
Peter Davison <davison.pl@gmail.com>
2026-03-21 17:37:36 -0400

    Bug fix for scoreboard order reversal from ESPN

--- lambdas/UpdateResults/lambda_function.py
+++ lambdas/UpdateResults/lambda_function.py
@@ -3,6 +3,7 @@ Lambda that updates results.json by fetching scoreboard data from S3.
 Loops through scoreboard events, finds matching id in results.ids, updates status/scores/result.
 """
 from bracket_common import bracket_utils
+from bracket_common import tournament as trn
 from blr_common import blr_utils
 import json
 from datetime import datetime
@@ -28,10 +29,13 @@ def _event_to_game_data(ev):
         game_status = "IN_PROGRESS"
 
     competitors = ev["competitions"][0]["competitors"]
-    score0 = int(competitors[0]["score"]) if len(competitors) > 0 and competitors[0].get("score") else 0
-    score1 = int(competitors[1]["score"]) if len(competitors) > 1 and competitors[1].get("score") else 0
+    score0 = int(competitors[0]["score"]) if competitors[0].get("score") else 0
+    score1 = int(competitors[1]["score"]) if competitors[1].get("score") else 0
 
-    return {"status": game_status, "scores": [score0, score1]}
+    id0 = competitors[0]["id"] if competitors[0].get("id") else ""
+    id1 = competitors[1]["id"] if competitors[1].get("id") else ""
+
+    return {"status": game_status, "scores": [score0, score1], "team_ids": [id0, id1]}
 
 
 def lambda_handler(event, context):
@@ -53,7 +57,6 @@ def lambda_handler(event, context):
     results_key = year + "/results.json"
     results_dict = blr_utils.read_file_s3(bucket, results_key)
 
-    # TODO: change this to a live call to ESPN scoreboard
     try:
         print(f"Fetching scoreboard data from url {SCOREBOARD_URL}")
         with urlopen(SCOREBOARD_URL, timeout=10) as resp:
@@ -69,10 +72,21 @@ def lambda_handler(event, context):
     statuses = results_dict["statuses"]
     scores = results_dict["scores"]
     results = results_dict["results"]
+    espn_ids = []
 
-    num_updated = 0
+
+    teams_key = year + "/teams.json"
+    teams = blr_utils.read_file_s3(bucket,teams_key)
+    team_ids = [t.get("espn_id", None) for t in teams]
+
+    for i, (i_upper, i_lower) in enumerate(make_absolute_bracket(results)):
+        espn_ids.append([
+            team_ids[i_upper] if i_upper is not None else None,
+            team_ids[i_lower] if i_lower is not None else None
+        ])
 
     print(f"Found {len(scoreboard_data.get('events', []))} events in scoreboard")
+    num_updated = 0
 
     # Loop through scoreboard; for each entry find its id in results.ids and update
     for ev in scoreboard_data.get("events", []):
@@ -85,16 +99,24 @@ def lambda_handler(event, context):
             continue
 
         data = _event_to_game_data(ev)
+
+        # order of competitors in espn scoreboard is based on seeds, order in results is based on bracket structure
+        # so we need to flip the scoreboard if the order is different
+        if data["team_ids"][0] == espn_ids[i][0]:
+            score = data["scores"]
+        else:
+            score = data["scores"][::-1]
+
         old_status = statuses[i]
         statuses[i] = data["status"]
         if data["status"] == "COMPLETE":
-            scores[i] = data["scores"]
+            scores[i] = score
             results[i] = 0 if scores[i][0] > scores[i][1] else 1
         elif data["status"] == "NOT_STARTED":
             scores[i] = [None, None]
             results[i] = None
         elif data["status"] == "IN_PROGRESS":
-            scores[i] = data["scores"]
+            scores[i] = score
             results[i] = None
 
         if statuses[i] != old_status:
@@ -112,3 +134,55 @@ def lambda_handler(event, context):
             bracket_utils.trigger_sync_sns(year, cid.replace(" ", "").lower())
 
     return {"body": f"Successfully updated results for year {year}. {num_updated} games updated."}
+
+
+def make_absolute_bracket(results, picks=None):
+    """
+    Converts flat list of 0/1 results and/or picks into flat list of upper/lower absolute indices into teams
+
+    - results must start from round0 and continue through round i (or be empty)
+    - if present picks must start at round i+1 (or zero if results empty) and continue through final round.
+
+    Returns list of absolute upper/lower indices for each game, None if there is no result/pick for game
+
+    Returns None if something goes wrong (TODO: do something else here)
+    """
+    # rel_inds is 0/1 result/pick from previous game
+    if picks is not None:
+        rel_inds = results + picks
+    else:
+        rel_inds = results
+
+    if len(rel_inds) != trn.NUMGAMES:
+        return None
+
+    abs_inds = []
+
+    # create flat list of bracket indices
+    for i in range(trn.NUMGAMES):
+        i_prev_upper, i_prev_lower = trn.PREV_GAME[i]
+
+        if i_prev_upper is None:
+            i_upper = 2 * i
+            i_lower = 2 * i + 1
+
+        else:
+            abs_upper = abs_inds[i_prev_upper]
+            rel_upper = rel_inds[i_prev_upper]
+
+            if rel_upper is None:
+                i_upper = None
+            else:
+                i_upper = abs_upper[rel_upper]
+
+            abs_lower = abs_inds[i_prev_lower]
+            rel_lower = rel_inds[i_prev_lower]
+
+            if rel_lower is None:
+                i_lower = None
+            else:
+                i_lower = abs_lower[rel_lower]
+
+        abs_inds.append((i_upper, i_lower))
+
+    return abs_inds
